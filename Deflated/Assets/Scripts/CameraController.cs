e13d0f4ecfa67c99cfee396c7e3ecb33517ad823
André Håland <andre.haland@gmail.com>
2017-10-20 12:08:05 +0200

    Rotate camera by clicking and dragging mouse

--- Deflated/Assets/Scripts/CameraController.cs
+++ Deflated/Assets/Scripts/CameraController.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
+
+    /* Offset between player and camera */
     private Vector3 offset;
+
+    /*Draggin camera by clicking and dragging mouse*/
+    private bool mouseButtonHeldDown = false;
+    private Vector3 mousePosOnClick;
+    private Vector3 lastMousePos;
+    public float speed = 0.025f;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         offset = transform.position - player.transform.position;
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseButtonHeldDown = true;
+            mousePosOnClick = Input.mousePosition;
+            lastMousePos = mousePosOnClick;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseButtonHeldDown = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
         transform.position = player.transform.position + offset;
-	}
+
+        if (mouseButtonHeldDown)
+        {
+            Vector3 currentMousePos = Input.mousePosition;
+            if (lastMousePos != currentMousePos)
+            {
+                Vector3 diff = lastMousePos - currentMousePos;
+                transform.Rotate(diff.y * speed, diff.x * speed, 0, Space.World);
+            }
+            lastMousePos = currentMousePos;
+        }
+        
+    }
 }
