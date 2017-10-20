5664524c9f94ffe3d307976fae59771bc4883cc1
Erlend <erlend061090@gmail.com>
2017-10-20 14:47:12 +0200

    Initial commit

---
+++ Deflated/Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    /* Offset between player and camera */
+    private Vector3 offset;
+
+    /*Draggin camera by clicking and dragging mouse*/
+    private bool mouseButtonHeldDown = false;
+    private Vector3 mousePosOnClick;
+    private Vector3 lastMousePos;
+    public float speed = 0.025f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	}
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseButtonHeldDown = true;
+            mousePosOnClick = Input.mousePosition;
+            lastMousePos = mousePosOnClick;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseButtonHeldDown = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+
+        if (mouseButtonHeldDown)
+        {
+            Vector3 currentMousePos = Input.mousePosition;
+            if (lastMousePos != currentMousePos)
+            {
+                Vector3 diff = lastMousePos - currentMousePos;
+                transform.Rotate(diff.y * speed, diff.x * speed, 0, Space.World);
+            }
+            lastMousePos = currentMousePos;
+        }
+        
+    }
+}
---
+++ Deflated/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10;
+    public float jumpSpeed = 200.0f;
+    private bool isGrounded = true;
+    private bool inWater = false;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (isGrounded == true && inWater == false)
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        if (inWater == false)
+        {
+            rb.AddForce(movement * speed);
+        }
+        else
+        {
+            rb.AddForce(movement * (speed / 3));
+        }
+    }
+
+    /*
+     *  Function which adds force upwards, thus producing a jump.
+     *  The variable jumpSpeed can be altered to produce effects
+     *  of buffs, pickups etc.  
+     */
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpSpeed);
+    }
+
+    /*
+     *  Functions 'OnCollisionEnter' and 'OnCollisionExit' detects
+     *  if the game character is on or off the ground. Used to prevent
+     *  weird in-air behaviour such as double jumps.
+     */
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            isGrounded = true;
+        }
+        if (collision.gameObject.name == "Water")
+        {
+            inWater = true;
+        }
+    }
+
+    // Function to detect if the game character is off the ground
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            isGrounded = false;
+        }
+        if (collision.gameObject.name == "Water")
+        {
+            inWater = false;
+        }
+    }
+}
