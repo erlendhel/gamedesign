385f35a27c907514be067d78a8d8fd7c28e0c216
André Håland <andre.haland@gmail.com>
2017-10-23 13:04:49 +0200

    Mouse scroll wheel can now be used to zoom in and out

--- Deflated/Assets/Scripts/CameraController.cs
+++ Deflated/Assets/Scripts/CameraController.cs
@@ -4,73 +4,69 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    /*The transform of the player*/
+    public Transform playerTransform;
 
-    public GameObject player;
+    /*Rotating camera by clicking and dragging mouse*/
+    private bool mouseButtonIsHeldDown = false;
 
-    /* Offset between player and camera */
-    private Vector3 offset;
-
-
-    public Transform lookAt;
-    public Transform cameraTransform;
-
+    //The endpoints of how far the camera can rotate vertically
     private const float Y_MIN = 0.0f;
     private const float Y_MAX = 50f;
 
-    private Camera cam;
+    /*The endpoints of how far the camera can zoom in and out*/
+    private const float SCROLL_MIN = 1.5f;
+    private const float SCROLL_MAX = 50f;
 
-    private float distance = 7.0f;
+    /*Variables used to control the behaviour of the camera*/
+    private float distanceToCamera = 7.0f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
+    private float scrollAmount = 0.0f;
     private float turnSpeedX = 4.0f;
-    private float turnSpeedY = 2.0f;
-
-
-    /*Rotating camera by clicking and dragging mouse*/
-    private bool mouseButtonHeldDown = false;
-
-    
-
-    // Use this for initialization
-    void Start()
-    {
-        cameraTransform = transform;
-        cam = Camera.main;
-        offset = new Vector3(player.transform.position.x, player.transform.position.y + 2f, player.transform.position.z + 7.0f);
-    }
+    private float turnSpeedY = 1.0f;
+    private float scrollSpeed = 4.0f;
 
 
     private void Update()
     {
+        //Register when left mousebutton is pushed down
         if (Input.GetMouseButtonDown(0))
         {
-            mouseButtonHeldDown = true;
+            mouseButtonIsHeldDown = true;
         }
+        //Register when left mousebutton is released
         if (Input.GetMouseButtonUp(0))
         {
-            mouseButtonHeldDown = false;
+            mouseButtonIsHeldDown = false;
         }
 
-        if (mouseButtonHeldDown)
+        if (mouseButtonIsHeldDown)
         {
+            //Get mouse coordinates
             currentX += Input.GetAxis("Mouse X") * turnSpeedX;
             currentY += Input.GetAxis("Mouse Y") * turnSpeedY;
 
+            //Disable camera from rotating below Y-MIN and above Y-MAX
             currentY = Mathf.Clamp(currentY, Y_MIN, Y_MAX);
         }
+
+        scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * -1f;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        
-        Vector3 dir = new Vector3(0, 0, -distance);
+        //Set distance based on the player scrolling mouse wheel
+        scrollAmount *= (distanceToCamera * 0.3f);
+        distanceToCamera += scrollAmount;
+        distanceToCamera = Mathf.Clamp(distanceToCamera, SCROLL_MIN, SCROLL_MAX);
 
+        //Rotate camera and look at the player
+        Vector3 direction = new Vector3(0, 0, -distanceToCamera);
         Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-
-        cameraTransform.position = lookAt.position + rotation * dir;
-        cameraTransform.LookAt(lookAt.position);
-        
+        transform.position = playerTransform.position + rotation * direction;
+        transform.LookAt(playerTransform.position);
     }
 
 }
