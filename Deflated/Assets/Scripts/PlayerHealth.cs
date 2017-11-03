ea04623790fb459e19e412eae904bd0afb8bd9af
Erlend <erlend061090@gmail.com>
2017-11-03 22:09:03 +0100

    save

--- Deflated/Assets/Scripts/PlayerHealth.cs
+++ Deflated/Assets/Scripts/PlayerHealth.cs
@@ -11,6 +11,10 @@ public class PlayerHealth : MonoBehaviour {
     private float healthDecrease = 2.0f;
     public float currentHealth;
 
+    private float smallIncrease = 10.0f;
+    private float mediumIncrease = 15.0f;
+    private float bigIncrease = 20.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -26,4 +30,14 @@ public class PlayerHealth : MonoBehaviour {
             currentHealth = 100.0f;
         }
 	}
+
+    private void OnTriggerEnter(Collider bubble) {
+        if (bubble.gameObject.CompareTag("SmallBubble")) {
+            currentHealth += smallIncrease;
+        } else if (bubble.gameObject.CompareTag("MediumBubble")) {
+            currentHealth += mediumIncrease;
+        } else if (bubble.gameObject.CompareTag("LargeBubble")) {
+            currentHealth += bigIncrease;
+        }
+    }
 }
