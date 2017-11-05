0cfe84557ee319f3408e139d864bbf2eb0c0aac5
Erlend <erlend061090@gmail.com>
2017-11-05 14:48:10 +0100

    Forced fix

---
+++ Deflated/Assets/Standard Assets/Environment/Water/Scripts/ScrollUV.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour {
+    public float WF_speed = 0.75f;
+    public Renderer WF_renderer;
+
+    private void Start() {
+        WF_renderer = GetComponent<Renderer>();
+    }
+
+    private void Update() {
+        float TextureOffset = Time.time * WF_speed;
+        WF_renderer.material.SetTextureOffset("_MainTex", new Vector2(0, TextureOffset));
+    }
+}
