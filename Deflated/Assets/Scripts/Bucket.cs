bbb790b288439ef0bae219ab04c8eafbb641ed61
André Håland <andre.haland@gmail.com>
2017-11-06 10:40:43 +0100

    Fixed weight-door prefab

---
+++ Deflated/Assets/Scripts/Bucket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour {
+
+    private float yStartPosition;
+    private Rigidbody bucketRigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        yStartPosition = transform.position.y;
+        bucketRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.y < yStartPosition)
+        {
+            bucketRigidBody.AddForce(0, 1, 0);
+        }
+
+	}
+}
