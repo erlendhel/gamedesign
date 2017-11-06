23ab7a3c5cb37b3bba6b1147e983da6b6a817b5c
Erlend <erlend061090@gmail.com>
2017-11-06 19:15:59 +0100

    Fix of GUI commit, created healthbar, actionbar and label for currency

--- Deflated/Assets/Scripts/PlayerInventory.cs
+++ Deflated/Assets/Scripts/PlayerInventory.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour {
 
     // Variables indicating if the player has a pickup or not
-    public bool hasMinimizer, hasMaximizer, hasDecreaseGrav, hasIncreaseGrav,
+    public static bool hasMinimizer, hasMaximizer, hasDecreaseGrav, hasIncreaseGrav,
         hasInstantSpeed, hasTimeBasedSpeed, hasInstantJump, hasTimeBasedJump,
             hasKey = false;
 
     private void OnTriggerEnter(Collider pickup) {
         if (pickup.gameObject.CompareTag("Minimizer")) {
             hasMinimizer = true;
+            GUIController.guiController.minimizerButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("Maximizer")) {
             hasMaximizer = true;
+            GUIController.guiController.maximizerButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("IncreaseGrav")) {
             hasIncreaseGrav = true;
+            GUIController.guiController.increaseGravButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("DecreaseGrav")) {
             hasDecreaseGrav = true;
+            GUIController.guiController.decreaseGravButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("InstantJump")) {
             hasInstantJump = true;
+            GUIController.guiController.instantJumpButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("TimeBasedJump")) {
             hasTimeBasedJump = true;
+            GUIController.guiController.timedJumpButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("SpeedInstant")) {
             hasInstantSpeed = true;
+            GUIController.guiController.instantSpeedButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("SpeedTime")) {
             hasTimeBasedSpeed = true;
+            GUIController.guiController.timedSpeedButton.interactable = true;
         } else if (pickup.gameObject.CompareTag("Key")) {
             hasKey = true;
         }
