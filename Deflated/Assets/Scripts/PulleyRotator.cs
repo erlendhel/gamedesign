deb912fe4310304e4414d22c5c10104ff1ffca50
André Håland <andre.haland@gmail.com>
2017-11-08 10:33:37 +0100

    Added missing weight-door scripts

---
+++ Deflated/Assets/Scripts/PulleyRotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulleyRotator : MonoBehaviour {
+
+    public Transform objectConnectedTo;
+    public bool clockwise = false;
+
+    private float lastDoorY;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        lastDoorY = objectConnectedTo.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float currentDoorY = objectConnectedTo.transform.position.y;
+        float toRotate = (currentDoorY - lastDoorY) * -1;
+
+        if (clockwise)
+            toRotate *= -1;
+
+        transform.Rotate(0, toRotate * 100, 0);
+
+        lastDoorY = objectConnectedTo.position.y;
+	}
+}
