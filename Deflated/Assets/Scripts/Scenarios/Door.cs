63dc56d4d3aed56003097cf9d5408f0981fcf3fe
André Håland <andre.haland@gmail.com>
2017-11-09 17:39:10 +0100

    Started work on pulley sound when door is raised, works on way down, but not the way up

--- Deflated/Assets/Scripts/Scenarios/Door.cs
+++ Deflated/Assets/Scripts/Scenarios/Door.cs
@@ -7,6 +7,7 @@ public class Door : MonoBehaviour {
     public Transform bucket;
 
     private float lastBucketY;
+    private bool isMoving = false;
 
 	// Use this for initialization
 	void Start ()
@@ -20,8 +21,30 @@ public class Door : MonoBehaviour {
         float currentBucketY = bucket.transform.position.y;
         float yToTranslate = (currentBucketY - lastBucketY) * -1;
 
+        // Used to determine if a sound effect should be played. 
+        // Bigger than or equal to 0.01f to ensure no audio at start of scene
+        if (yToTranslate >= 0.001f)
+        {
+            Debug.Log("isMoving = true");
+            isMoving = true;
+        }
+        else
+        {
+            Debug.Log("isMoving = true");
+            isMoving = false;
+        }
+            
+
+        Debug.Log(yToTranslate);
+
+        // If the door is moving, and the SFX hasn't started playing, start playing SFX
+        if (isMoving && !AudioManager.instance.IsPlaying("PulleyOperating"))
+            AudioManager.instance.Play("PulleyOperating");
+        // If door has stopped moving, and the SFX is still playing, stop SFX
+        else if (!isMoving && AudioManager.instance.IsPlaying("PulleyOperating"))
+            AudioManager.instance.Stop("PulleyOperating");
+
         transform.Translate(0, yToTranslate, 0);
-        
         lastBucketY = bucket.transform.position.y;
     }
 }
