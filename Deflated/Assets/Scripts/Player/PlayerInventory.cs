0ec66e10ca886d2cbe8546585764084bbe7c6f6e
André Håland <andre.haland@gmail.com>
2017-11-10 16:50:38 +0100

    Mechanics of multiple key parts now works. Next up is UI

--- Deflated/Assets/Scripts/Player/PlayerInventory.cs
+++ Deflated/Assets/Scripts/Player/PlayerInventory.cs
@@ -2,40 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerInventory : MonoBehaviour {
 
-    // Variables indicating if the player has a pickup or not
+    // Variables indicating if the player has a pickup or not. Public because they are written to in ActionController
     public static bool hasMinimizer, hasMaximizer, hasDecreaseGrav, hasIncreaseGrav,
-        hasInstantSpeed, hasTimeBasedSpeed, hasInstantJump, hasTimeBasedJump,
-            hasKey = false;
+        hasInstantSpeed, hasTimeBasedSpeed, hasInstantJump, hasTimeBasedJump = false;
+
+    // Variable indicating if the player has a key (used in scenarios where the player picks up the whole key)
+    private static bool hasKey = false;
+    
+    // Variables indicating which keyparts the player has picked up
+    private static bool hasFirstKeyPart, hasSecondKeyPart, hasThirdKeyPart = false;
+
 
     private void OnTriggerEnter(Collider pickup) {
-        if (pickup.gameObject.CompareTag("Minimizer")) {
+        if (pickup.gameObject.CompareTag("Minimizer"))
+        {
             hasMinimizer = true;
             GUIController.guiController.minimizerButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("Maximizer")) {
+        }
+        else if (pickup.gameObject.CompareTag("Maximizer"))
+        {
             hasMaximizer = true;
             GUIController.guiController.maximizerButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("IncreaseGrav")) {
+        }
+        else if (pickup.gameObject.CompareTag("IncreaseGrav"))
+        {
             hasIncreaseGrav = true;
             GUIController.guiController.increaseGravButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("DecreaseGrav")) {
+        }
+        else if (pickup.gameObject.CompareTag("DecreaseGrav"))
+        {
             hasDecreaseGrav = true;
             GUIController.guiController.decreaseGravButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("InstantJump")) {
+        }
+        else if (pickup.gameObject.CompareTag("InstantJump"))
+        {
             hasInstantJump = true;
             GUIController.guiController.instantJumpButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("TimeBasedJump")) {
+        }
+        else if (pickup.gameObject.CompareTag("TimeBasedJump"))
+        {
             hasTimeBasedJump = true;
             GUIController.guiController.timedJumpButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("SpeedInstant")) {
+        }
+        else if (pickup.gameObject.CompareTag("SpeedInstant"))
+        {
             hasInstantSpeed = true;
             GUIController.guiController.instantSpeedButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("SpeedTime")) {
+        }
+        else if (pickup.gameObject.CompareTag("SpeedTime"))
+        {
             hasTimeBasedSpeed = true;
             GUIController.guiController.timedSpeedButton.interactable = true;
-        } else if (pickup.gameObject.CompareTag("Key")) {
+        }
+        else if (pickup.gameObject.CompareTag("Key"))
+        {
             hasKey = true;
         }
+        else if (pickup.gameObject.CompareTag("FirstKeyPart"))
+        {
+            hasFirstKeyPart = true;
+            Debug.Log("HasFirstKeyPart");
+        }
+        else if (pickup.gameObject.CompareTag("SecondKeyPart"))
+        {
+            hasSecondKeyPart = true;
+            Debug.Log("HasSecondKeyPart");
+        }
+        else if (pickup.gameObject.CompareTag("ThirdKeyPart"))
+        {
+            hasThirdKeyPart = true;
+            Debug.Log("HasThirdKeyPart");
+        }
+    }
+
+    // Used to check on collisions with locks that require one whole key to be picked up
+    public static bool HasKey()
+    {
+        return hasKey;
+    }
+
+    // Used to check on collison with locks that require all parts of a key to be picked up
+    public static bool HasAllKeyParts()
+    {
+        return hasFirstKeyPart && hasSecondKeyPart && hasThirdKeyPart;
     }
 }
---
+++ Deflated/Assets/Scripts/TopKeyPartAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopKeyPartAnimator : MonoBehaviour {
+
+    private float lowerBoundary;
+    private float upperBoundary;
+    private bool goingUpwards = true;
+    private float increment = 0.009f;
+
+    // Use this for initialization
+    void Start()
+    {
+        upperBoundary = transform.position.x + 0.025f;
+        lowerBoundary = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.x >= upperBoundary)
+            goingUpwards = false;
+        else if (transform.position.x <= lowerBoundary)
+            goingUpwards = true;
+
+        if (goingUpwards)
+            transform.Translate(increment, 0, 0);
+        else
+            transform.Translate(increment * -1f, 0, 0);
+
+        transform.Rotate(increment * 100f, 0, 0);
+    }
+}
