c16a0633e66490e5765a88eedcd052caeb1947db
Erlend <erlend061090@gmail.com>
2017-11-12 22:32:37 +0100

    Added initial button script and model

---
+++ Deflated/Assets/Scripts/Interactive Elements/ButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+    private bool isPressed = false;
+    private Vector3 endPosition;
+
+	// Use this for initialization
+	void Start () {
+        endPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player") && !isPressed) {
+            transform.position = new Vector3(transform.position.x, 
+                transform.position.y, transform.position.z - 0.3f);
+            isPressed = true;
+        }
+    }
+}
