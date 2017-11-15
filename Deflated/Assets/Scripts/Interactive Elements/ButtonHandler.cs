e709b866d816c131111e558efd105c3cdbd712db
Erlend <erlend061090@gmail.com>
2017-11-15 08:07:13 +0100

    applied on collide animation to button, buggy

--- Deflated/Assets/Scripts/Interactive Elements/ButtonHandler.cs
+++ Deflated/Assets/Scripts/Interactive Elements/ButtonHandler.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class ButtonHandler : MonoBehaviour {
 
     private bool isPressed = false;
+    private Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Player") && !isPressed) {
+            anim.enabled = true;
             isPressed = true;
         }
     }
