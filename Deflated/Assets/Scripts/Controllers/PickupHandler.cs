d90be63b340b3029c75ff0e955e2537692d77807
André Håland <andre.haland@gmail.com>
2017-12-11 14:33:26 +0100

    First commit on winter wonderland

--- Deflated/Assets/Scripts/Controllers/PickupHandler.cs
+++ Deflated/Assets/Scripts/Controllers/PickupHandler.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class PickupHandler : MonoBehaviour {
 
+    private MeshRenderer[] renderers;
+    private Collider collider;
+    private float respawnTime = 30f;
+
+    private void Start(){
+        collider = GetComponent<Collider>();
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        print(renderers.Length);
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
-            gameObject.SetActive(false);
+            // Disable pickup and all meshrenderes in pickup 
+            collider.enabled = false;
+            foreach (MeshRenderer render in renderers) {
+                render.enabled = false;
+            }
+
+            // Start timer to enable meshrenderes and colliders after certain time limit
+            StartCoroutine("PickupTimer");
+        }
+    }
+
+    IEnumerator PickupTimer() {
+        yield return new WaitForSeconds(respawnTime);
+        // Enable collider and meshrenderers after 30 seconds
+        collider.enabled = true;
+        foreach (MeshRenderer render in renderers) {
+            render.enabled = true;
         }
     }
 }
---
+++ Deflated/Assets/Scripts/Scenarios/KnockableFence.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockableFence : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Get physic data from player on collision
+            Rigidbody player = collision.gameObject.GetComponent<Rigidbody>();
+            float playerVelocity = Vector3.Magnitude(player.velocity);
+            float kineticEnergy = ((player.mass * Mathf.Pow(playerVelocity,2)) / 2);
+
+            Debug.Log("Kinetic energy on collison with fence: " + kineticEnergy);
+            
+            // If the player hits the fence with a kinetic force of more than 40J
+            if (kineticEnergy > 10f)
+            {
+                // Turn of all constraints, so the fence will fall down
+                rb.constraints = RigidbodyConstraints.None;
+            }
+        }
+    }
+}
