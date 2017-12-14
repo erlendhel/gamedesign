0e5a762be8016af0237dc3e4c5b5410ea729684d
Nina Heglund <ninaheglund@hotmail.no>
2017-12-14 13:13:59 +0100

    desert island

---
+++ Deflated/Assets/Scripts/MagnetFieldHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetFieldHandler : MonoBehaviour {
+
+    public GameObject gameObject;
+    public RigidBody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+   
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.GameObject.CompareTag("Player"))
+        {
+            rb = collision.GetComponent<RigidBody>();
+            rb.AddForce(10.0f, 10.0f, 10.0f);
+        }
+    }
+}
