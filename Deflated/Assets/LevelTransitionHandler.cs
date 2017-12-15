1b1b1d61cd6ede2a87d26f790238ddd938c87394
André Håland <andre.haland@gmail.com>
2017-12-15 20:16:59 +0100

    Teleporter on first island

--- Deflated/Assets/LevelTransitionHandler.cs
+++ Deflated/Assets/LevelTransitionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelTransitionHandler : MonoBehaviour {
 
@@ -8,9 +9,11 @@ public class LevelTransitionHandler : MonoBehaviour {
     public PlayerController playerController;
     public ParticleSystem particles;
     private bool isEnabled = false;
-    public GameObject keyPart;
     Material[] m_Material;
     Color green = Color.green;
+
+    public int sceneIndex;
+    private bool doneAnimating = false;
 
     // Use this for initialization
     void Start () {
@@ -21,12 +24,15 @@ public class LevelTransitionHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (PlayerInventory.hasThirdKeyPart && !isEnabled) {
+        if (PlayerInventory.hasFirstKeyPart && !isEnabled) {
             isEnabled = true;
             m_Material[2].color = green;
             m_Material[4].color = green;
             m_Material[5].color = green;
         }
+
+        if (doneAnimating)
+            SceneManager.LoadSceneAsync(sceneIndex);
 	}
 
     private void OnTriggerStay(Collider other) {
@@ -40,5 +46,8 @@ public class LevelTransitionHandler : MonoBehaviour {
     IEnumerator Transition() {
         yield return new WaitForSeconds(4);
         print("Change scene");
+        StopAllCoroutines();
+        doneAnimating = true;
+
     }
 }
