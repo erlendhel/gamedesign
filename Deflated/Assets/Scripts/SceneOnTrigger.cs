e989a45f7b569f03c719a4b66f33ff3ab447714a
André Håland <andre.haland@gmail.com>
2017-12-15 19:45:42 +0100

    Started on scene loaders

---
+++ Deflated/Assets/Scripts/SceneOnTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneOnTrigger : MonoBehaviour {
+
+    public int sceneIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(sceneIndex);
+        }
+    }
+}
